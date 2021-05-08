77babe1e9648c810c7aa463644dd3972199b2200
jbenet <nycflasher@yahoo.com>
2021-05-08 10:25:43 -0400

    a little CSS

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav>
-      <img className ="photo" src="https://i.imgur.com/D8Bz3HO.jpg" alt="sloth" />
+      <img className="photo" src="https://i.imgur.com/D8Bz3HO.jpg" alt="sloth" />
+      <div className="nav">
       <Link to="/">Sloth JS products</Link>
-      <Link to="/new">Submit new recipe here</Link>
+        <Link to="/new">New Product Submission Form</Link>
+        </div>
     </nav>
   )
 }

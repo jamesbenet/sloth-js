1c80964825fa0c428ac4612e9b1a1aa7fcf4dad5
jbenet <nycflasher@yahoo.com>
2021-05-08 08:32:39 -0400

    hm

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className ="photo" src="https://i.imgur.com/D8Bz3HO.jpg" alt="sloth" />
+      </header>
+    </div>
+  );
+}
+
+export default App;

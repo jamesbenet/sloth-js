e106420d556c8a82657c59df2327d19d2ad923a2
jbenet <nycflasher@yahoo.com>
2021-05-10 08:42:41 -0400

    get resolved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Route } from "react-router-dom";
 import Form from "./components/Form";
 import Nav from "./components/Nav";
-import './App.css';
+import { baseURL, config } from "./services";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [toggleFetch, setToggleFetch] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      // const url = "https://api.airtable.com/v0/appzGBcPBzpuGidxc/slothJS?maxRecords=3&view=Grid%20view";
+      const resp = await axios.get(baseURL, config);
+      // console.log(resp.data);
+      console.log(resp.data.records);
+      setProducts(resp.data.records);
+    };
+    fetchProducts();
+    console.log(products);
+    // console.log({products.fields});
+  }, [toggleFetch]);
+  console.log(products);
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+     
       <Nav></Nav>
       <Route exact path="/">
-        <main>
-          
+       
+        <main>{products.map((product) => (
+          <p>{product.fields.name}</p>
+        ))}
         </main>
       </Route>
+      <div>
       <Route path="/new">
         <Form />
       </Route>
+      </div>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav>
-      <img className="photo" src="https://i.imgur.com/D8Bz3HO.jpg" alt="sloth" />
-      <div className="nav">
-      <Link to="/">Sloth JS products</Link>
-        <Link to="/new">New Product Submission Form</Link>
+      <div class="box">
+         <div class="image-wrapper">
+            <img src="https://i.imgur.com/N2pBvHFb.png" alt="sloth" />
+         </div>
+        <div className="nav">
+          <Link to="/">Sloth JS products</Link>
+          <Link to="/new">New Product Submission Form</Link>
         </div>
-    </nav>
+      </div>  
+     </nav> 
   )
 }
+      
 export default Nav;
